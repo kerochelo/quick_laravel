19ae744396b9fce25cbd2586041f822436a18305
Takahiro Kanno <kerochelo@gmail.com>
2023-02-23 20:53:57 +0900

    :+1:category model and migration

---
+++ src/app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'deleted_at'
+    ];
+
+    public function getRouteKeyName()
+    {
+        $route_name = Request::route()->getName();
+        if (str_starts_with($route_name, 'admin')) {
+            return 'id';
+        }
+        return 'slug';
+    }
+}
