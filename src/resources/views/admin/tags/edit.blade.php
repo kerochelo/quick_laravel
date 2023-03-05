90962afb7bdd7a4b2c8bb73f2a6258324de02641
Takahiro Kanno <kerochelo@gmail.com>
2023-03-05 16:16:40 +0900

    :+1::bug:user controller and view and fix update method form

--- src/resources/views/admin/tags/edit.blade.php
+++ src/resources/views/admin/tags/edit.blade.php
@@ -20,18 +20,19 @@
   <section class="content">
     <div class="box">
       <form action="{{ route('admin.tags.update', $tag) }}" method="POST">
-      @csrf
-      <div class="box-body">
-        <div class="form-group string required tag_name">
-          <label class="control-label string required" for="tag_name"><abbr title="required">*</abbr> タグ名</label>
-          <input class="form-control string required" type="text" value="{{ $tag->name }}" name="name" id="tag_name" />
+        @method('PUT')
+        @csrf
+        <div class="box-body">
+          <div class="form-group string required tag_name">
+            <label class="control-label string required" for="tag_name"><abbr title="required">*</abbr> タグ名</label>
+            <input class="form-control string required" type="text" value="{{ $tag->name }}" name="name" id="tag_name" />
+          </div>
+          <div class="form-group string required tag_slug">
+            <label class="control-label string required" for="tag_slug"><abbr title="required">*</abbr> スラッグ</label>
+            <input class="form-control string required" type="text" value="{{ $tag->slug }}" name="slug" id="tag_slug" />
+          </div>
         </div>
-        <div class="form-group string required tag_slug">
-          <label class="control-label string required" for="tag_slug"><abbr title="required">*</abbr> スラッグ</label>
-          <input class="form-control string required" type="text" value="{{ $tag->slug }}" name="slug" id="tag_slug" />
-        </div>
-      </div>
-      <div class="box-footer"><input type="submit" name="commit" value="更新する" class="btn btn-primary" data-disable-with="更新する" /></div>
+        <div class="box-footer"><input type="submit" name="commit" value="更新する" class="btn btn-primary" data-disable-with="更新する" /></div>
       </form>
     </div>
   </section>
